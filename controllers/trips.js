b9918fb493dab4c3f96278cbadb2e9aa76b66033
gabeantonio <antoniogabe99@gmail.com>
2022-08-11 16:38:17 -0700

    created and rendered add new trip page

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,9 +1,14 @@
 const Trip = require('../models/trip');
 
 module.exports = {
-    index
+    index,
+    new: newTrip
 }
 
 function index(req, res) {
     res.render('trips/my-trips.ejs')
+}
+
+function newTrip(req, res) {
+    res.render('trips/new-trip.ejs');
 }
--- routes/trips.js
+++ routes/trips.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const tripController = require('../controllers/trips')
 
 router.get('/', tripController.index);
+router.get('/new', tripController.new)
 
 module.exports = router;
