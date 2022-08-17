1b855cbae674d9f2a969b0c95a7abdfcd6890771
gabeantonio <antoniogabe99@gmail.com>
2022-08-17 16:47:23 -0700

    refactor create function

--- controllers/events.js
+++ controllers/events.js
@@ -18,19 +18,34 @@ function newEvent(req, res) {
 
 function create(req, res) {
     Trip.findById(req.params.id, function(err, tripDocument) {
-        console.log(tripDocument, '<--- EVENT CREATION!!');
-        let events;
-        let itinerary;
-        for (let i = 0; i < tripDocument.itinerary.length; i++) {
-            itinerary = tripDocument.itinerary[i]._id;
-            day = tripDocument.itinerary[i].day;
-            events = tripDocument.itinerary[i].events;
-            events.push(req.body);
-            console.log(events, '<--- EVENTS!!!')
-            console.log(tripDocument, '<---- TRIP WITH EVENTS???');
-            tripDocument.save(function(err) {
-                res.render('days/show-day.ejs', {title: 'Trip', trip: tripDocument});
-            })
-        }
-    })
+      // Update req.body to contain user info
+    req.body.userId = req.user._id;
+    req.body.userName = req.user.name;
+      // Add the comment
+    trip.itinerary.events.push(req.body);
+    trip.save(function(err) {
+        res.redirect(`/trips/${trip._id}/day/${trip.itinerary.day._id}`);
+    });
+    });
 }
+
+
+
+// function create(req, res) {
+//     Trip.findById(req.params.id, function(err, tripDocument) {
+//         console.log(tripDocument, '<--- EVENT CREATION!!');
+//         let events;
+//         let itinerary;
+//         for (let i = 0; i < tripDocument.itinerary.length; i++) {
+//             itinerary = tripDocument.itinerary[i]._id;
+//             day = tripDocument.itinerary[i].day;
+//             events = tripDocument.itinerary[i].events;
+//             events.push(req.body);
+//             console.log(events, '<--- EVENTS!!!')
+//             console.log(tripDocument, '<---- TRIP WITH EVENTS???');
+//             tripDocument.save(function(err) {
+//                 res.render('days/show-day.ejs', {title: 'Trip', trip: tripDocument});
+//             })
+//         }
+//     })
+// }
