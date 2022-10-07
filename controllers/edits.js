33211960a74d672801aaed704b2b60a9c26f59ff
gabeantonio <antoniogabe99@gmail.com>
2022-10-07 01:17:17 -0700

    reset styling

--- controllers/edits.js
+++ controllers/edits.js
@@ -21,19 +21,9 @@ function updatePlan(req, res) {
         let dayDocument = tripDocument.itinerary.id(req.params.dayId);
         let index = tripDocument.itinerary.map(itinerary => itinerary.id).indexOf(req.params.dayId);
         let eventIndex = tripDocument.itinerary[index].events.map(event => event.id).indexOf(req.params.eventId);
-        console.log(req.params.eventId, '<----- EVENT ID!!!!!!')
-        console.log(eventIndex, '<---- EVENT INDEX!!!!!!');
-        console.log(index, '<----- INDEX!!!!!');
-        console.log(tripDocument, '<----- TRIP DOC')
-        console.log(tripDocument.itinerary[index].events[eventIndex], '<---- BEFORE')
+        
         tripDocument.itinerary[index].events[eventIndex] = req.body;
-        console.log(tripDocument.itinerary[index].events[eventIndex], '<---- AFTER');
-        // console.log(dayDocument, '<---- THIS IS THE DAY YOU WERE ON')
-        // console.log(req.body, '<---- WHAT WAS SUBMITTED');
-        // dayDocument.events.shift();
-        // console.log(dayDocument, '<---- DELETED EVENT IN DAY DOCUMENT');
-        // dayDocument.events.unshift(req.body);
-        // console.log(dayDocument, '<---- ADDED NEW ACTIVITES')
+        
         tripDocument.save(function (err) {
             if (err) {
                 res.redirect(`/trips/${tripDocument._id}`)
@@ -44,86 +34,17 @@ function updatePlan(req, res) {
     })
 }
 
-// function create(req, res) {
-
-//     Trip.findById(req.params.id, function (err, tripDocument) {
-//         console.log(tripDocument, '<----- TRIP!!!!!!');
-//         console.log(req.params.dayId, '<------ REQ PARAMS DAY ID')
-//         let dayDocument = tripDocument.itinerary.id(req.params.dayId)
-//         console.log(dayDocument, '<---- DAY DOCUMENT')
-//         console.log(req.params.dayId, '<---- DAY ID')
-//         dayDocument.events.push(req.body);
-//         tripDocument.save(function (err) {
-//             if (err) {
-//                 res.redirect(`/trips/${tripDocument._id}`)
-//             } else {
-//                 console.log(tripDocument, '<---- SAVED TRIP')
-//                 res.redirect(`/trips/${tripDocument._id}/day/${dayDocument._id}`)
-//             }
-//         })
-//     })
-// }
-
-
-
-
-
-
-
-
-
-// function update(req, res) {
-//     // Note the cool "dot" syntax to query on the property of a subdoc
-//     Book.findOne({'comments._id': req.params.id}, function(err, book) {
-//       // Find the comment subdoc using the id method on Mongoose arrays
-//       // https://mongoosejs.com/docs/subdocs.html
-//       const commentSubdoc = book.comments.id(req.params.id);
-//       // Ensure that the comment was created by the logged in user
-//       if (!commentSubdoc.userId.equals(req.user._id)) return res.redirect(`/books/${book._id}`);
-//       // Update the text of the comment
-//       commentSubdoc.text = req.body.text;
-//       // Save the updated book
-//       book.save(function(err) {
-//         // Redirect back to the book's show view
-//         res.redirect(`/books/${book._id}`);
-//       });
-//     });
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function create(req, res) {
-
-//     Trip.findById(req.params.id, function (err, tripDocument) {
-//         console.log(tripDocument, '<----- TRIP!!!!!!');
-//         console.log(req.params.dayId, '<------ REQ PARAMS DAY ID')
-//         let dayDocument = tripDocument.itinerary.id(req.params.dayId)
-//         console.log(dayDocument, '<---- DAY DOCUMENT')
-//         console.log(req.params.dayId, '<---- DAY ID')
-//         dayDocument.events.push(req.body);
-//         tripDocument.save(function (err) {
-//             if (err) {
-//                 res.redirect(`/trips/${tripDocument._id}`)
-//             } else {
-//                 console.log(tripDocument, '<---- SAVED TRIP')
-//                 res.redirect(`/trips/${tripDocument._id}/day/${dayDocument._id}`)
-//             }
-//         })
+// function updatePlan(req, res) {
+//     Trip.findById(req.params.id, function(err, tripDocument) {
+//         let dayDocument = tripDocument.itinerary.id(req.params.dayId);
+//         let specificEvent = dayDocument.events.id(req.params.eventId);
+//         console.log(dayDocument, '<--- DAY DOCUMENT!');
+//         console.log(req.body, '<--- THE CHANGE I WANT TO MAKE')
+//         console.log(dayDocument.events.id(req.params.eventId), '<--- THE CURRENT EVENT IM ON')
+//         specificEvent.remove();
+//         specificEvent = req.body;
+//         console.log(specificEvent, '<--- SPECIFIC EVENT!')
+//         console.log(dayDocument, '<--- DAY DOCUMENT AFTER!')
+        
 //     })
 // }
